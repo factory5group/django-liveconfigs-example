763a6b02d860623a1d215c5ba764b8b9222644c7
Denis Dudnik <Denis.Dudnik@gmail.com>
2024-08-26 13:35:36 +0300

    fix dublicate from INSTALLED_APPS

--- liveconfigs_example/settings.py
+++ liveconfigs_example/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "import_export",
     "django_extensions",
     "django_celery_beat",
     "simpleapp.apps.SimpleappConfig",
